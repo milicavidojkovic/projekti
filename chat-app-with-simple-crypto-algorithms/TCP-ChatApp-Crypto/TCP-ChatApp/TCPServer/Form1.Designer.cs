a1acebf80173ccce38b3bf686f0f14b487624446
milicavidojkovic <147850204+milicavidojkovic@users.noreply.github.com>
2024-04-11 18:08:46 +0200

    Add files via upload

---
+++ chat-app-with-simple-crypto-algorithms/TCP-ChatApp-Crypto/TCP-ChatApp/TCPServer/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace TCPServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.serverLab = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lstClientIp = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(554, 34);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(113, 29);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // serverLab
+            // 
+            this.serverLab.AutoSize = true;
+            this.serverLab.Location = new System.Drawing.Point(233, 9);
+            this.serverLab.Name = "serverLab";
+            this.serverLab.Size = new System.Drawing.Size(78, 20);
+            this.serverLab.TabIndex = 1;
+            this.serverLab.Text = "Username:";
+            this.serverLab.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(233, 35);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(299, 27);
+            this.txtIP.TabIndex = 2;
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(233, 78);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInfo.Size = new System.Drawing.Size(434, 247);
+            this.txtInfo.TabIndex = 3;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(233, 370);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(315, 27);
+            this.txtMessage.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(233, 347);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Message:";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(554, 368);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(113, 29);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lstClientIp
+            // 
+            this.lstClientIp.FormattingEnabled = true;
+            this.lstClientIp.ItemHeight = 20;
+            this.lstClientIp.Location = new System.Drawing.Point(14, 81);
+            this.lstClientIp.Name = "lstClientIp";
+            this.lstClientIp.Size = new System.Drawing.Size(197, 244);
+            this.lstClientIp.TabIndex = 7;
+            this.lstClientIp.SelectedIndexChanged += new System.EventHandler(this.lstClientIp_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Addressbook:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(233, 81);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(133, 24);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Paralelni prikaz:";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 421);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstClientIp);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.serverLab);
+            this.Controls.Add(this.btnStart);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TCP Chat App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label serverLab;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox lstClientIp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ chat-app-with-simple-crypto-algorithms/TCP-ChatApp-Crypto/TCP-ChatApp/TCPServer/Form1.cs
@@ -0,0 +1,297 @@
+﻿using SuperSimpleTcp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCPServer
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> addressBook = new Dictionary<string, string>();
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        SimpleTcpServer server;
+        SimpleTcpClient client1;
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnSend.Enabled = false;
+            
+            addressBook.Add("Kika", "127.0.0.2:9000");
+            addressBook.Add("Srecko", "127.0.0.3:9000");
+            addressBook.Add("Mika", "127.0.0.4:9000");
+
+           
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            
+            if (server.IsListening)
+            {
+                if (!string.IsNullOrEmpty(txtMessage.Text) && lstClientIp.SelectedItem != null)
+                {
+                    client1.Connect();
+                    client1.Send(txtIP.Text + ": " + Coding(txtMessage.Text));
+
+                   
+                    txtInfo.Text += $"Me: {txtMessage.Text}{Environment.NewLine}";
+                    txtMessage.Text = string.Empty;
+                }
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtIP.Text) && addressBook.ContainsKey(txtIP.Text))
+            {
+                txtIP.Enabled=false;
+                server = new SimpleTcpServer(addressBook[txtIP.Text]);
+                server.Events.ClientConnected += Events_ClientConnected;
+                server.Events.ClientDisconnected += Events_ClientDisconnected;
+                server.Events.DataReceived += Events_DataReceived;
+                server.Start();
+                txtInfo.Text += $"Starting...{Environment.NewLine}";
+                btnStart.Enabled = false;
+                btnSend.Enabled = true;
+                var filteredKeys = addressBook.Where(pair => pair.Key != (txtIP.Text)).Select(pair => pair.Key);
+
+                lstClientIp.Items.AddRange(filteredKeys.ToArray());
+
+
+            }
+
+
+        }
+
+        private void Events_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                
+                string[] data = (Encoding.UTF8.GetString(e.Data)).Split(' ');
+                txtInfo.Text += $"{data[0]}: {Decoding(data[1])}{Environment.NewLine}";
+
+                if (radioButton1.Checked == true)
+                {
+                    txtInfo.Text += $"{data[0]}: {(data[1])}{Environment.NewLine}";
+                    radioButton1.Checked = false;
+                }
+                //Kod korisnika koji prima poruke treba da bude cekiran paralelni prikaz pre primanja poruke
+
+
+
+
+            });
+        }
+
+        private void Events_ClientDisconnected(object sender, ConnectionEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                txtInfo.Text += $"{e.IpPort}disconnected.{Environment.NewLine}";
+                lstClientIp.Items.Remove(e.IpPort);
+            });
+
+        }
+
+        private void Events_ClientConnected(object sender, ConnectionEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                //txtInfo.Text += $"{e.IpPort}connected.{Environment.NewLine}";
+                //lstClientIp.Items.Add(e.IpPort);
+
+            });
+
+        }
+      
+        private string Coding(string tekst)
+        {
+            tekst = tekst.ToLower().Replace("j", "i").Replace(" ", "");
+            string alphabet = "phqgmeaylnofdxkrcvszwbuti";
+            int[] nizI = new int[tekst.Length];
+            int[] nizJ = new int[tekst.Length];
+            int count = 0;
+            char[,] matrix = new char[5, 5];
+            char[] code = new char[tekst.Length];
+            int[] script = new int[2 * tekst.Length];
+            bool found = false;
+            int i, j;
+            int ostatak = tekst.Length % 5;
+            int brojac = tekst.Length / 5;
+
+
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = alphabet[i * 5 + j];
+                }
+            }
+
+            //prolazak kroz tekst
+            while (tekst.Length > count)
+            {
+
+                found = false;
+                i = 0;
+                while (i < 5 && found == false)
+                {
+                    j = 0;
+                    while (j < 5 && found == false)
+                    {
+                        if (tekst[count] == matrix[i, j])
+                        {
+                            nizI[count] = i;
+                            nizJ[count] = j;
+                            found = true;
+                        }
+                        j++;
+                    }
+                    i++;
+                }
+
+
+
+                count++;
+            }
+            //kreiranje finalnog niza brojeva
+            for (i = 0; i < brojac; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    script[i * 10 + j] = nizI[i * 5 + j];
+                    script[i * 10 + j + 5] = nizJ[i * 5 + j];
+
+                }
+            }
+            for (i = 0; i < ostatak; i++)
+            {
+                script[brojac * 10 + i] = nizI[brojac * 5 + i];
+                script[brojac * 10 + i + ostatak] = nizJ[brojac * 5 + i];
+
+            }
+            count = 0;
+            //kodiranje
+            for (i = 0; i < tekst.Length; i++)
+            {
+
+                code[i] = matrix[script[count], script[++count]];
+                count++;
+            }
+
+            return new string(code);
+        }
+
+        private string Decoding(string code)
+        {
+            int count = 0, i, j, pom;
+            bool found = false;
+            code = code.ToLower().Replace("j", "i").Replace(" ", "");//preventivno
+            string alphabet = "phqgmeaylnofdxkrcvszwbuti";
+            int[] nizI = new int[code.Length];
+            int[] nizJ = new int[code.Length];
+            char[,] matrix = new char[5, 5];
+            int[] script = new int[2 * code.Length];
+            int ostatak = code.Length % 5;
+            int brojac = code.Length / 5;
+            char[] tekst = new char[code.Length];
+
+            //kreiranje matrice azbuke
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = alphabet[i * 5 + j];
+                }
+            }
+
+            //pronalazenje vrednosti iz matrice
+            while (code.Length > count)
+            {
+
+                found = false;
+                i = 0;
+                while (i < 5 && found == false)
+                {
+                    j = 0;
+                    while (j < 5 && found == false)
+                    {
+                        if (code[count] == matrix[i, j])
+                        {
+                            pom = count * 2;
+                            script[pom] = i;
+                            script[pom + 1] = j;
+                            found = true;
+                        }
+                        j++;
+                    }
+                    i++;
+                }
+                count++;
+            }
+
+            //delimo niz script u delove od po 10
+            for (i = 0; i < brojac; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    nizI[i * 5 + j] = script[i * 10 + j];
+                    nizJ[i * 5 + j] = script[i * 10 + j + 5];
+
+                }
+            }
+            for (i = 0; i < ostatak; i++)
+            {
+                nizI[brojac * 5 + i] = script[brojac * 10 + i];
+                nizJ[brojac * 5 + i] = script[brojac * 10 + i + ostatak];
+
+            }
+            //kodiranje
+            for (i = 0; i < code.Length; i++)
+            {
+
+                tekst[i] = matrix[nizI[i], nizJ[i]];
+
+            }
+            return new string(tekst);
+            //return code;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+            //radioButton1.Checked = !radioButton1.Checked;
+            
+        }
+
+        private void lstClientIp_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            client1 = new SimpleTcpClient(addressBook[lstClientIp.SelectedItem.ToString()]);
+        }
+    }
+}
